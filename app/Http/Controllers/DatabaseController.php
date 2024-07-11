518a57b1fe6281d4b349cdf5e7cc93cade3d55f2
Celso Nery <celso.nery@gmail.com>
2024-07-11 12:24:42 -0300

    feat: Sincroniza as bases.

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -4,18 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\DatabaseRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class DatabaseController extends Controller
 {
-    protected array $databases = [];
-    protected string $baseProd = '';
-    protected string $baseDev = '';
+    protected $databases = [];
+    protected $baseProd = '';
+    protected $baseDev = '';
 
     public function index(): JsonResponse
     {
-        $process = Process::fromShellCommandline("sudo -u postgres psql -l | awk '{print $1}' | egrep -v 'List|Name|--|\||\(|dev|hml'");
+//        $process = Process::fromShellCommandline("psql -h 10.0.10.129 -U postgres -l | awk '{print $1}' | egrep -v 'List|Name|--|\||\(|dev|hml'");
+        Log::debug("Listando bancos...");
+
+        $process = Process::fromShellCommandline("/usr/bin/psql -h 10.0.10.129 -U postgres -l | awk '{print $1}' | egrep -v 'List|Name|--|\||\(|dev|hml'");
         $process->run();
 
         // executes after the command finishes
@@ -36,9 +40,12 @@ class DatabaseController extends Controller
 
     public function verify(DatabaseRequest $request): JsonResponse
     {
+        $this->baseProd = $request->database;
         $this->baseDev = $request->database . '_dev';
 
-        $process = Process::fromShellCommandline("sudo -u postgres psql -l | awk '{print $1}' | grep {$this->baseDev}");
+        Log::debug("Procurando se banco {$this->baseDev} já existe...");
+
+        $process = Process::fromShellCommandline("/usr/bin/psql -h 10.0.10.129 -U postgres -l | grep {$this->baseDev}");
         $process->run();
 
         if (!$process->isSuccessful()) {
@@ -51,7 +58,9 @@ class DatabaseController extends Controller
     private function sinc(bool $drop): JsonResponse
     {
         if ($drop) {
-            $process = Process::fromShellCommandline("sudo -u postgres psql -c \"DROP DATABASE {$this->baseDev} with (force)\"");
+            Log::debug("Banco {$this->baseDev} já existe removendo...");
+
+            $process = Process::fromShellCommandline("/usr/bin/psql -h 10.0.10.129 -U postgres -c \"DROP DATABASE {$this->baseDev} with (force)\"");
             $process->run();
 
             if (!$process->isSuccessful()) {
@@ -60,20 +69,27 @@ class DatabaseController extends Controller
         }
 
         // Recria base de dados
-        $process = Process::fromShellCommandline("sudo -u postgres psql -c \"CREATE DATABASE {$this->baseDev}\"");
+        $drop ? Log::debug("Recriando banco {$this->baseDev}...") : Log::debug("Banco {$this->baseDev} ainda não existe, criando...");
+
+        $process = Process::fromShellCommandline("/usr/bin/psql -h 10.0.10.129 -U postgres -c \"CREATE DATABASE {$this->baseDev}\"");
         $process->run();
 
         if (!$process->isSuccessful()) {
             return response()->json(['message' => "Não foi possível criar {$this->baseDev}!"], 404);
         }
 
+        Log::debug("Sincronizando: {$this->baseProd} para {$this->baseDev}");
+
         // Realiza o sincronismo
-        $process = Process::fromShellCommandline("sudo -u postgres pg_dump -v {$this->baseProd} | sudo -u postgres psql {$this->baseDev}");
-        $process->run();
+        $process = Process::fromShellCommandline("/usr/bin/pg_dump -h 10.0.10.129 -U postgres -v {$this->baseProd} | /usr/bin/psql -h 10.0.10.129 -U postgres {$this->baseDev}");
+        $process->setTimeout(0)->run();
 
         if (!$process->isSuccessful()) {
             return response()->json(['message' => "Não foi possível sincronizar {$this->baseDev}! - {$process->getErrorOutput()}"], 404);
         }
+
+        Log::debug("Sincronizado: {$this->baseProd} para {$this->baseDev} com sucesso!");
+        Log::debug("---");
 
         return response()->json(['message' => "Base {$this->baseDev} sincronizada com sucesso!"], 200);
     }
