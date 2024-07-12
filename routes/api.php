71159c7cf12085ef267a2bef6673d9a93c54c1d2
Celso Nery <celso.nery@gmail.com>
2024-07-12 08:41:29 -0300

    feat: Limpa api.php

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\DatabaseController;
 use Illuminate\Support\Facades\Route;
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+
 
 //Route::middleware('auth:sanctum')->group(function () {
     Route::get('/databases', [DatabaseController::class, 'index'])->name('database.index');
