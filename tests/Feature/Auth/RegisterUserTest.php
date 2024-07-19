9520e6472e6f8d01f0600d8a603034e3bcdf53a8
Celso Nery <celso.nery@gmail.com>
2024-07-19 09:56:45 -0300

    chore: Atualiza CHANGELOG e reports

--- tests/Feature/Auth/RegisterUserTest.php
+++ tests/Feature/Auth/RegisterUserTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\Role;
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
